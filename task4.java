6a60ffb739305dfd0cb997ddb57c97f2bc11ca80
omkar jadhav <officialomkarjadhav@gmail.com>
2023-03-11 16:38:11 +0000

    task4 completed

---
+++ task4.java
@@ -0,0 +1,24 @@
+import java.util.Comparator;
+import java.util.TreeMap;
+
+public class task4 {
+    public static void main(String[] args) {
+        TreeMap<String,String> t=new TreeMap<>();
+        t.put("c1", "Red");
+        t.put("c4", "Blue");
+        t.put("c2", "Green");
+        t.put("c3", "Yellow");
+         System.out.println(t);
+        class sortkey implements Comparator<String> {
+
+            @Override
+            public int compare(String o1, String o2) {
+                // TODO Auto-generated method stub
+                return o1.compareTo(o2);
+
+            }
+            
+
+        }
+    }
+}
